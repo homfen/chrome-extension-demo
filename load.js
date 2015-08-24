4f3a8472b48e23736d9bf93fc68a0c0dc18dd228
homfen <homfen@qq.com>
2015-08-25 02:37:35 +0800

    onActivated

--- load.js
+++ load.js
@@ -1,13 +1,22 @@
-console.log("loaded");
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {  
-    var action = request.action;
-    switch(action){
-        case "showTitle":
-            showTitle();
-            break;
-        default:
-            console.log("unknown action");
-            break;
+if(!window.loaded){
+    
+    console.log("loaded");
+    window.loaded = true;
+    
+    function showTitle(){
+        alert(document.title);
     }
-});
+
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {  
+        var action = request.action;
+        switch(action){
+            case "showTitle":
+                showTitle();
+                break;
+            default:
+                console.log("unknown action");
+                break;
+        }
+    });
+    
+}
